bba720b6395e5179ba243e2776870c008cd7d1af
Daniel Rees <daniel.rees18@gmail.com>
2018-10-25 14:31:09 -0400

    Add SerializedName to PhxMessage (#18)
    * Expose OkHttpClient and clear connection on failure
    
    * Added SerializedName to PhxMessage. Updated ReadME

--- src/main/kotlin/org/phoenixframework/PhxMessage.kt
+++ src/main/kotlin/org/phoenixframework/PhxMessage.kt
@@ -1,15 +1,26 @@
 package org.phoenixframework
 
+import com.google.gson.annotations.SerializedName
+
 class PhxMessage(
         /** The unique string ref. Empty if not present */
+        @SerializedName("ref")
         val ref: String = "",
+
         /** Property "topic" is never used */
+        @SerializedName("topic")
         val topic: String = "",
+
         /** Property "topic" is never used */
+        @SerializedName("event")
         val event: String = "",
+
         /** Property "topic" is never used */
+        @SerializedName("payload")
         val payload: Payload = HashMap(),
+
         /** The ref sent during a join event. Empty if not present. */
+        @SerializedName("join_ref")
         val joinRef: String? = null) {
 
 
