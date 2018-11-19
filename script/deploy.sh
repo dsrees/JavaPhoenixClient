d54b00bedb24e1202d32380688a34413e47e1b42
Daniel Rees <drees@clustertruck.com>
2018-11-19 10:02:44 -0500

    fixed deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,10 +3,10 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew bintrayUpload --stacktrace
+  ./gradlew build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew build
+  ./gradlew bintrayUpload --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   ./gradlew bintrayUpload --stacktrace
