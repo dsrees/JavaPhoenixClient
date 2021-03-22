600be627643d65278b2ad0a79dd1bb5d8243cc9f
dsrees <daniel.rees18@gmail.com>
2021-03-22 14:10:38 -0400

    Start a travis build

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # This script will build the project.
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  echo -e '#### Build for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG'] ####'
-./gradlew bintrayUpload --stacktrace
-else
-  echo -e '#### Build for Test => Branch ['$TRAVIS_BRANCH'] Pull Request ['$TRAVIS_PULL_REQUEST'] ####'
-  ./gradlew clean build jacocoTestReport
-fi 
+./gradlew clean publish --stacktrace
+# if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+#   echo -e '#### Build for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG'] ####'
+# ./gradlew bintrayUpload --stacktrace
+# else
+#   echo -e '#### Build for Test => Branch ['$TRAVIS_BRANCH'] Pull Request ['$TRAVIS_PULL_REQUEST'] ####'
+#   ./gradlew clean build jacocoTestReport
+# fi 
